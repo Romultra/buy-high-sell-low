3d9c00ca9e510ee912a853a37b7d5e9a0f215d43
EmiliaSalazar <emiliaisalazarcretton@gmail.com>
2025-03-11 21:37:37 +0100

    finished updating nonprosumer optimization function & updates in 2.1

--- UsefulFunctions.py
+++ UsefulFunctions.py
@@ -188,18 +188,15 @@ def Optimizer_NonProsumer(params, price):
     p_d = cp.Variable(n)
     X   = cp.Variable(n)
 
-    ### Define the flow variable ###
-    flow = p_c - p_d
-
     ### Define the profit function ###
-    profit = cp.sum( f_minus @ price - f_plus @ price)
+    profit = cp.sum(p_c @ price - p_d @ price)
     
     ### Add constraints ###
     constraints = [p_c >= 0, 
                    p_d >= 0, 
                    p_c <= params['Pmax'], 
                    p_d <= params['Pmax']]
-    constraints += [X >= 0, X <= params['Cmax']]
+    constraints += [X >= params['Cmin'], X <= params['Cmax']]
     constraints += [X[0]==params['C_0'] + p_c[0]*params['n_c'] - p_d[0]/params['n_d']]
     constraints += [X[1:] == X[:-1] + p_c[1:]*params['n_c'] - p_d[1:]/params['n_d']]
     constraints += [X[n-1]>=params['C_n']]
